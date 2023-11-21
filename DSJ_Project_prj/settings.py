b9f46095e0cfdaabc1a9ab89f93a7cce33151922
soooong7 <dlthdal07@gmail.com>
2023-11-21 14:40:51 +0900

    앱 생성

--- DSJ_Project_prj/settings.py
+++ DSJ_Project_prj/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DSJ_main',
+    'Login',
+    'Notice',
+    'Products',
+    'Purchase',
+    'QnA',
 ]
 
 MIDDLEWARE = [
--- DSJ_main/apps.py
+++ DSJ_main/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DsjConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DSJ'
+    name = 'DSJ_main'
