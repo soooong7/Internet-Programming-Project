c21b22b47299951924f86d873ac53234ac9f0b4c
Jang Eunsun <20211234@duksung.ac.kr>
2023-11-22 22:17:40 +0900

    Feat: "Set post title form"

--- Notice/models.py
+++ Notice/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
