7448a62db1b390ecc03804ba86e7f61d4c258ba4
Jang Eunsun <20211234@duksung.ac.kr>
2023-11-28 14:26:37 +0900

    Rename: "Rename app name and unify settings.py"

--- DSJ_Project_prj/settings.py
+++ DSJ_Project_prj/settings.py
@@ -38,12 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DSJ_main',
-    'Login',
+    'main',
+    'login',
     'notice',
-    'Products',
-    'Purchase',
-    'QnA',
+    'products',
+    'purchase',
+    'qna',
 ]
 
 MIDDLEWARE = [
@@ -122,10 +122,13 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
