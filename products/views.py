d94f3fe31de57b2315b189cbbfa28510e209deff
minji <minj20@duksung.ac.kr>
2023-11-29 23:46:03 +0900

    FEAT : 상세페이지에서 사이즈와 수량 선택할 수 있도록 변경 DOCS: views.py, purchase_detail.html 변경

--- products/views.py
+++ products/views.py
@@ -9,6 +9,7 @@ def index(request):
     size_categories = SizeCategory.objects.all()
     quantity_categories = NumberCategory.objects.all()
 
+
     return render(request, 'products/purchase_detail.html', {'quantity_categories': quantity_categories , 'size_categories': size_categories})
     # return render(request, 'products/purchase_detail.html')
 
