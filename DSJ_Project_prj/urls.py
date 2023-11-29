30e3a0d14982776311064692e31d94f46aa5ac89
minji <minj20@duksung.ac.kr>
2023-11-30 00:47:47 +0900

    views.py, purchase.detail 수정2

--- DSJ_Project_prj/urls.py
+++ DSJ_Project_prj/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('main/', include('main.urls')),
+    path('main/', include('main.urls')),
     path('products/',  include('products.urls')),
 ]
 
