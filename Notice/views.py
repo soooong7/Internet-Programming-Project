2a97930127a2e968f24b3a9a2351dd17b3401b26
Jang Eunsun <20211234@duksung.ac.kr>
2023-11-30 17:33:17 +0900

    Fix: "Fix url error"

--- Notice/views.py
+++ Notice/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-def notice(request):
+def index(request):
     return render(
         request,
         'notice/index.html'
--- notice/migrations/0002_auto_20231122_2355.py
+++ notice/migrations/0002_auto_20231122_2355.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0001_initial'),
+        ('notice', '0001_initial'),
     ]
 
     operations = [
