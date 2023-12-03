d9a4db2d3f7e8031ad36eda53435bf43c4e5ce3e
Jang Eunsun <20211234@duksung.ac.kr>
2023-12-04 00:35:29 +0900

    Remove: "Remove code for multiple images"

--- notice/admin.py
+++ notice/admin.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
-from .models import Post, Image
+from .models import Post
 
 admin.site.register(Post)
-admin.site.register(Image)
-
-class ImageInline(admin.TabularInline):
-    model = Post.images.through
-
-class PostAdmin(admin.ModelAdmin):
-    inlines = [ImageInline]
-
-admin.site.register(Post, PostAdmin)
--- notice/models.py
+++ notice/models.py
@@ -4,10 +4,7 @@ class Post(models.Model):
     title = models.CharField(max_length=40)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    notice_image = models.ManyToManyField('image.Image')
+    notice_image = models.ImageField(upload_to='notice/images/%y/%m/%d/', blank=True, default='default_image.png')
 
     def __str__(self):
         return f'[{self.pk}] {self.title}'
-
-class Image(models.Model):
-    image = models.ImageField(upload_to='notice/images/%y/%m/%d/')
