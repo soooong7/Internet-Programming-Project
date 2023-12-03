724438e6688a328c74c958d426ab7f6d77193ff4
Jang Eunsun <20211234@duksung.ac.kr>
2023-12-03 23:57:54 +0900

    Fix: "Fix post error"

--- notice/models.py
+++ notice/models.py
@@ -4,7 +4,7 @@ class Post(models.Model):
     title = models.CharField(max_length=40)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    notice_image = models.ImageField(upload_to='notice/images/%y/%m/%d/', blank=True, default='default_image.png')
+    notice_image = models.ImageField(upload_to='notice/images/%y/%m/%d/', blank=True,)
 
     def __str__(self):
         return f'[{self.pk}] {self.title}'
