1de97436868c2eb795606df1d61a0b7748f0c309
Jang Eunsun <20211234@duksung.ac.kr>
2023-12-04 00:18:57 +0900

    Fix: "Add multiple images in notice"

--- notice/admin.py
+++ notice/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Image
 
 admin.site.register(Post)
+admin.site.register(Image)
+
+
+class ImageInline(admin.TabularInline):
+    model = Post.images.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
--- notice/models.py
+++ notice/models.py
@@ -4,7 +4,10 @@ class Post(models.Model):
     title = models.CharField(max_length=40)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    notice_image = models.ImageField(upload_to='notice/images/%y/%m/%d/', blank=True,)
+    notice_image = models.ManyToManyField(Image)
 
     def __str__(self):
         return f'[{self.pk}] {self.title}'
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='notice/images/%y/%m/%d/', blank=True,)
