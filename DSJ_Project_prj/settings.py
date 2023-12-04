df3af2af96c0f0380e921d60c657efe8dd672c1b
dbsdPdnjs <yewonyoon@gmail.com>
2023-12-04 15:43:53 +0900

    please

--- DSJ_Project_prj/settings.py
+++ DSJ_Project_prj/settings.py
@@ -122,9 +122,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = '/qna/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'qna/static')]
-
+STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
--- DSJ_Project_prj/urls.py
+++ DSJ_Project_prj/urls.py
@@ -15,23 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('notice/', include('notice.urls')),
     path('main/', include('main.urls')),
-    #path('products/',  include('products.urls')),
-    #path('products/',  include('products.urls')),
+    path('login/', include('login.urls')),
     path('qna/', include('qna.urls')),
-    #path('qna/qna_detail/', qna_detail, name='qna_detail'),
-]
+    path('products/', include('products.urls')),
+    ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
