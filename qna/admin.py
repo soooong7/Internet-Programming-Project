854e664cfca5f0af6cdb9dafccdc93bac1ef0af0
SONG <145523888+soooong7@users.noreply.github.com>
2023-12-04 15:26:47 +0900

    Remove: Delete qna directory

--- qna/admin.py
+++
@@ -1,4 +0,0 @@
-from .models import QnAPost
-from django.contrib import admin
-
-admin.site.register(QnAPost)
--- qna/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import ListView, DetailView
-from .models import QnAPost
-
-class PostList(ListView):
-    model = QnAPost
-    ordering = '-pk'
-
-class PostDetail(DetailView):
-    model = QnAPost
